8d66db8529f38a3b36b3883ae9401a53201902a5
jijihohococo <jijihohococo@gmail.com>
2021-12-15 20:35:21 +0630

    fix

--- src/Middleware/APIMiddleware.php
+++ src/Middleware/APIMiddleware.php
@@ -8,8 +8,7 @@ class APIMiddleware extends MainMiddleware{
 		$headers=getallheaders();
 
 		if(isset($headers['Content-Type']) && $headers['Content-Type']!=='application/json' ){
-			$notFound=new NotFound;
-			echo $notFound->show('405 - Only API Request is allowed',405);
+			echo NotFound::show('405 - Only API Request is allowed',405);
 			exit();
 		}
 		return $this->next();
--- src/Middleware/CSRFMiddleware.php
+++ src/Middleware/CSRFMiddleware.php
@@ -6,8 +6,7 @@ class CSRFMiddleware{
 
 	public function handle(){
 		if($_SERVER['REQUEST_METHOD']=='POST' && $_REQUEST['csrf_token']!==$_SESSION['csrf_token']){
-			$notFound=new NotFound;
-			echo $notFound->show('401 - CSRF Token Expired',401);
+			echo NotFound::show('401 - CSRF Token Expired',401);
 			exit();
 		}
 		return $this->next();
