ac091ed2b9ca312ae67f51441fd8408ff6569e93
jijihohococo <jijihohococo@gmail.com>
2021-12-18 21:57:51 +0630

    add exception

--- src/Container/DependencyInject.php
+++ src/Container/DependencyInject.php
@@ -7,7 +7,7 @@ class DependencyInject{
 	public function getConstructor($className,$functionName,$functionParameters){
 		$createdClass=$this->getObject($className);
 		if(!method_exists($createdClass, $functionName)){
-			throw new \Exception($functionName. " function is not exist in ".$className, 1);
+			throw new \Exception($functionName. " function is not exist in ".$className . " Class", 1);
 		}
 		return call_user_func_array([$createdClass,$functionName], $functionParameters);
 	}
@@ -24,6 +24,9 @@ class DependencyInject{
 	public function createObjects($parameters){
 		$objects=[];
 		foreach($parameters as $key => $parameter ){
+			if(!interface_exists($parameter)){
+				throw new \Exception($parameter . " Interface is not exist", 1);
+			}
 			$type= str_replace('Interface', '', (string)$parameter->getType());
 			$typeArray=explode('\\' , $type );
 			if(isset($typeArray[0])){
