3b598dcbac920ab9138b6bae8c8eec0a2ce73274
jijihohococo <jijihohococo@gmail.com>
2021-12-23 13:31:34 +0630

    improve

--- src/Middleware/CSRFMiddleware.php
+++ src/Middleware/CSRFMiddleware.php
@@ -5,7 +5,13 @@ use JiJiHoHoCoCo\IchiRoute\UI\NotFound;
 class CSRFMiddleware extends MainMiddleware{
 
 	public function handle(){
-		if($_SERVER['REQUEST_METHOD']=='POST' && $_REQUEST['csrf_token']!==$_SESSION['csrf_token']){
+		if($_SERVER['REQUEST_METHOD']=='POST' && ((isset($_SESSION['csrf_token']) && $_REQUEST['csrf_token']!==$_SESSION['csrf_token']) || 
+			(!isset($_SESSION['csrf_token'])))){
+
+			if(isset($_SESSION['csrf_token'])){
+				unset($_SESSION['csrf_token']);
+			}
+			
 			echo NotFound::show('401 - CSRF Token Expired',401);
 			exit();
 		}
