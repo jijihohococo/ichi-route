cb5bad1b3a838cf8e132cd57820670c89c3d44b9
Lin Aung <jijihohococo@gmail.com>
2022-11-23 10:13:26 +0900

    add

--- src/Command/RouteCommand.php
+++ src/Command/RouteCommand.php
@@ -14,6 +14,10 @@ class RouteCommand{
 
 	private $resourceController=FALSE;
 
+	private $green="\033[0;32m";
+	private $red="\033[01;31m";
+	private $end=" \033[0m";
+
 	public function setMiddlewarePath(string $middlewarePath){
 		$this->middlewarePath=$middlewarePath;
 	}
@@ -171,23 +175,31 @@ class ".$createdFile." extends MainMiddleware{
 		}
 	}
 
+	public function successMessage(string $message){
+		return $this->green.$message.$this->end.PHP_EOL;
+	}
+
+	public function errorMessage(string $message){
+		return $this->red.$message.$this->end.PHP_EOL;
+	}
+
 	private function alreadyHave(string $createdFile,string $createdOption){
-		echo $createdFile . " ".$createdOption." is already created".PHP_EOL;
+		echo $this->errorMessage($createdFile . " ".$createdOption." is already created");
 		exit();
 	}
 
 	private function success(string $createdFile,string $createdOption){
-		echo $createdFile . " ".$createdOption." is created successfully".PHP_EOL;
+		echo $this->successMessage($createdFile . " ".$createdOption." is created successfully");
 		exit();
 	}
 
 	private function wrongCommand(){
-		echo "You type wrong command".PHP_EOL;
+		echo $this->errorMessage("You type wrong command");
 		exit();
 	}
 
 	private function createError(string $createdFile,string $createdOption){
-		echo "You can't create ". $createdFile . " " . $createdOption.PHP_EOL;
+		echo $this->errorMessage("You can't create ". $createdFile . " " . $createdOption);
 		exit();
 	}
 
